bb5e12aec79cf792f3d32fa26d3481e2af1ad989
RemdelBarela <remdeleiron.barela@tup.edu.ph>
2023-11-18 12:27:17 +0800

    login

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
-
-// const cookie = require('cookie-parser')
-// const cors = require('cors')
+const cookie = require('cookie-parser')
+const cors = require('cors')
 
 const auth = require('./routes/auth')
 
 // const products = require('./routes/product');
 // const order = require('./routes/order')
 
-// app.use(cors())
-// app.use(express.json({limit:'50mb'}));
-// app.use(express.urlencoded({limit: "50mb", extended: true }));
+app.use(cors())
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit: "50mb", extended: true }));
 
-// app.use(cookie());
+app.use(cookie());
 
 app.use('/api/v1', auth);
 
